8663182b525f1e3a6899481502f6053fa6f96371
Kaleb Franken <kaleb.franken7@gmail.com>
2023-07-27 18:06:01 -0600

    going to try a text glow feature

--- src/components/AppContent.jsx
+++ src/components/AppContent.jsx
@@ -1,5 +1,13 @@
 import ContentContainer from "./ContentContainer"
 
+function Item(props) {
+  return (
+    <div className="text-white rounded hover:shadow-lg hover:ring-4 hover:ring-blue-300 hover:ring-opacity-50">
+      {props.children}
+    </div>
+  )
+}
+
 function AppContent() {
   return (
     <div 
@@ -7,9 +15,9 @@ function AppContent() {
         className="w-[100vw] h-[100vh] absolute top-0 z-10 outline overflow-y-auto snap-y snap-mandatory"
     >
         <ContentContainer>
-          <div className="text-white text-2xl font-bold relative space-y-4">
-            <p> Software Developer </p>
-            <p> Name: Kaleb Franken </p>
+          <div className="text-white text-2xl font-bold relative space-y-4  ">
+            <Item> Software Developer </Item> 
+            <Item> Name: Kaleb Franken </Item>
             <p> Education: BS In Cloud Computing @Purdue Global </p>
             <p> Favorite Color: #0FFFFA </p>
           </div>
