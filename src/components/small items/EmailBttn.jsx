f0b590a1bbecda108e7cb7bd40934a1a8ed3a293
Kaleb Franken <kaleb.franken7@gmail.com>
2023-08-17 13:56:01 -0600

    updated email

--- src/components/small items/EmailBttn.jsx
+++ src/components/small items/EmailBttn.jsx
@@ -6,7 +6,7 @@ function EmailBttn() {
     
     return ( 
     <div className='fixed  top-[max(calc(2.2vw),25px)]  left-[calc(100%-90vw)] text-white  outline-white outline bg-black rounded-lg p-1  text-3xl  z-[30] drop-shadow-[0_1px_4px_rgba(255_255_255_/_80%)]'>
-        <a href="mailto:demo@demo.com">
+        <a href="mailto:kaleb.franken@icloud.com">
             <AiOutlineMail className='w-[calc(3vw-10px)] h-[calc(3vw-10px)] min-w-[20px] min-h-[20px] '/>
         </a>
     </div>
