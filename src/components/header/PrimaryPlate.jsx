5a506889473b65f2d57ed64d6dc4683de1f34ea9
Kaleb Franken <kaleb.franken7@gmail.com>
2023-08-17 18:58:49 -0600

    mobile test

--- src/components/header/PrimaryPlate.jsx
+++ src/components/header/PrimaryPlate.jsx
@@ -3,8 +3,8 @@ import { primaryHeaderJSVG } from "../jsx-paths/primaryHeaderJSVG"
 
 export default function PrimaryPlate({children}) {
   return (
-    <div className="absolute top-0 relative flex justify-center">
-      <div className="absolute top-0 w-[100vw] min-w-[1050px]" >
+    <div className="fixed top-0 relative flex justify-center">
+      <div className="top-0 w-[100vw] min-w-[1050px]" >
           {primaryHeaderJSVG}
       </div>
       <div className="absolute top-0 w-[40vw] min-w-[430px]">
