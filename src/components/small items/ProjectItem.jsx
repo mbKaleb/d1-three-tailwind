525260987c7da49013adde9994998c6e62b01a4a
Kaleb Franken <kaleb.franken7@gmail.com>
2023-12-01 16:11:32 -0700

    increased glow

--- src/components/small items/ProjectItem.jsx
+++ src/components/small items/ProjectItem.jsx
@@ -7,7 +7,7 @@ function ProjectItem({src, link }) {
 
     return (
         <div className="rounded w-fit h-fit text-white hover:[cursor:pointer] " onClick={onClickHandler} >
-            <img src={src} alt="non"  className="h-[20vh] rounded p-4 hover:drop-shadow-[0_1px_7px_rgba(255_255_255_/_60%)] "  ></img>
+            <img src={src} alt="non"  className="h-[20vh] rounded p-4 hover:drop-shadow-[0_1px_10px_rgba(255_255_255_/_80%)] "  ></img>
         </div>
     )
 }
