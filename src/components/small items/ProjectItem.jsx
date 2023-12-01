3ee6376840c48b642451c9b64ddb104625a0e3ba
Kaleb Franken <kaleb.franken7@gmail.com>
2023-12-01 15:13:44 -0700

    fixed items

--- src/components/small items/ProjectItem.jsx
+++ src/components/small items/ProjectItem.jsx
@@ -6,7 +6,7 @@ function ProjectItem({src, link }) {
     
 
     return (
-        <div className="rounded text-white hover:[cursor:pointer] " onClick={onClickHandler} >
+        <div className="rounded w-fit text-white hover:[cursor:pointer] " onClick={onClickHandler} >
             <img src={src} alt="non" width="260" height="200"  className=" rounded p-4 hover:drop-shadow-[0_1px_7px_rgba(255_255_255_/_60%)] "  ></img>
         </div>
     )
