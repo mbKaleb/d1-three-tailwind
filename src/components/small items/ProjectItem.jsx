2c468161bac5a36c5119f0f8affa2208dbdee4be
Kaleb Franken <kaleb.franken@icloud.com>
2026-04-09 23:50:36 -0700

    work around for icons, google is not being nice and their api is always stale or I am stupid

--- src/components/small items/ProjectItem.jsx
+++ src/components/small items/ProjectItem.jsx
@@ -1,9 +1,9 @@
 function ProjectItem({ link }) {
-    const favicon = `https://www.google.com/s2/favicons?domain=${link}&sz=128`
+    const favicon = `${link}/favicon.svg`
 
     return (
         <div className="rounded w-fit h-fit text-white hover:[cursor:pointer]" onClick={() => window.open(link, '_blank')}>
-            <img src={favicon} alt="favicon" className="h-[8vh] rounded p-2 hover:drop-shadow-[0_1px_10px_rgba(255_255_255_/_80%)]" />
+            <img src={favicon} alt="favicon" className="h-[8vh] rounded p-2 hover:drop-shadow-[0_0px_3px_rgba(255_255_255_/_80%)]" />
         </div>
     )
 }
